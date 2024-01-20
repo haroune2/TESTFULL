793e2142898072afd43fc4700a6786f1d9fd10c4
haroune_b <125916355+harouneboutlookfr@users.noreply.github.com>
2024-01-20 19:23:36 +0000

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Demo';
+  data = {}  as any;
+  constructor(private http: HttpClient) {
+    http.get('resource').subscribe(data => this.data = data);
+  }
+}
